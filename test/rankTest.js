6b0d01092e9d10ede3136e79e91be2f7d11acb8d
LISH5 <727904507@qq.com>
2020-08-27 17:35:59 +0800

    [homework][Shao] test: add test1:test rating with answer B

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,10 +1,52 @@
 const rankTest = require('ava');
+const {rating} = require('../src/rank');
 
-rankTest('foo', t => {
-  t.pass();
-});
+rankTest('test1:test rating with answer B', t => {
+    const voyage = {
+      zone: 'west-indies',
+      length: 10,
+    };
+    const history = [
+      {
+        zone: 'east-indies',
+        profit: 5,
+      },{
+        zone: 'west-indies',
+        profit: 15,
+      },{
+        zone: 'china',
+        profit: -2,
+      },
+      {
+        zone: 'west-africa',
+        profit: 7,
+      },
+    ];
+    const result = rating(voyage, history);
+    t.is(result, 'B');
+})
 
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
-});
+rankTest('test1:test rating with answer A', t => {
+    const voyage = {
+      zone: 'west-indies',
+      length: 10,
+    };
+    const history = [
+      {
+        zone: 'east-indies',
+        profit: 5,
+      },{
+        zone: 'west-indies',
+        profit: 15,
+      },{
+        zone: 'china',
+        profit: -2,
+      },
+      {
+        zone: 'west-africa',
+        profit: 7,
+      },
+    ];
+    const result = rating(voyage, history);
+    t.is(result, 'B');
+})
