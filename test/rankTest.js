f2d9e48058a825f1c5ad4ca89993739b2762010e
LISH5 <727904507@qq.com>
2020-08-27 23:18:20 +0800

    [homework][Shao] test: add test voyageProfitFactor with voyage zone is east-indies and voyage length > 14 and history.length > 8

--- test/rankTest.js
+++ test/rankTest.js
@@ -211,3 +211,42 @@ rankTest('test voyageProfitFactor with voyage zone is west-indies and voyage len
     const result = voyageProfitFactor(voyage, history);
     t.is(2, result);
 })
+
+rankTest('test voyageProfitFactor with voyage zone is east-indies and voyage length > 14 and history.length > 8', t => {
+    const voyage = {
+          zone: 'east-indies',
+          length: 10,
+        };
+    const history = [
+              {
+                zone: 'east-indies',
+                profit: 5,
+              },{
+                zone: 'west-indies',
+                profit: 15,
+              },{
+                zone: 'west-indies',
+                profit: -2,
+              },
+              {
+                zone: 'west-africa',
+                profit: 7,
+              },
+              {
+                zone: 'east-indies',
+                profit: 5,
+              },{
+                zone: 'west-indies',
+                profit: 15,
+              },{
+                zone: 'west-indies',
+                profit: -2,
+              },
+              {
+                zone: 'west-africa',
+                profit: 7,
+              },
+            ];
+    const result = voyageProfitFactor(voyage, history);
+    t.is(3, result);
+})
