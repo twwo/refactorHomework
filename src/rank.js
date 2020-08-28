c696d7de7fc289de703fef299305648dca7a3e40
LISH5 <727904507@qq.com>
2020-08-28 21:53:44 +0800

    [homework][Shao] refactor: rename

--- src/rank.js
+++ src/rank.js
@@ -72,10 +72,10 @@ function voyageProfitFactor (voyage, history) {
 }
 
 function rating (voyage, history) {
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > (vr + chr * 2)) {
+  const voyageProfitFactorResult = voyageProfitFactor(voyage, history);
+  const voyageRiskResult = voyageRisk(voyage);
+  const captainHistoryRiskResult = captainHistoryRisk(voyage, history);
+  if (voyageProfitFactorResult * 3 > (voyageRiskResult + captainHistoryRiskResult * 2)) {
     return 'A';
   }
   else {
