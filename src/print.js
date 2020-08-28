08a96722b3adc11ca014596a05352f1a39f6a869
LISH5 <727904507@qq.com>
2020-08-28 22:34:07 +0800

    [homework][Shao] refactor: extract method getTEXTResult

--- src/print.js
+++ src/print.js
@@ -1,13 +1,6 @@
 function printOwing (invoice) {
-  let outstanding = calculateOutstanding(invoice.borderSpacing);
   invoice.dueDate = getDueDate();
-  let result = '***********************\n'
-              + '**** Customer Owes ****\n'
-              + '***********************\n';
-  result += `name: ${invoice.customer}\n`
-            + `amount: ${outstanding}\n`
-            + `amount: ${invoice.dueDate.toLocaleDateString()}\n`;
-  return result;
+  return getTEXTResult(invoice, calculateOutstanding(invoice.borderSpacing));
 }
 
 function calculateOutstanding(borderSpacing) {
@@ -21,6 +14,16 @@ function calculateOutstanding(borderSpacing) {
 function getDueDate() {
     const today = new Date();
     return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function getTEXTResult(invoice, outstanding) {
+    let result = '***********************\n'
+                + '**** Customer Owes ****\n'
+                + '***********************\n'
+                + `name: ${invoice.customer}\n`
+                + `amount: ${outstanding}\n`
+                + `amount: ${invoice.dueDate.toLocaleDateString()}\n`;
+    return result;
 }
 
 module.exports = {
