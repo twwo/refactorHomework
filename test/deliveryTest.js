d71a923ce7711a15d1644e21595d7aef0f6023d8
LISH5 <727904507@qq.com>
2020-08-28 17:23:26 +0800

    [homework][Shao] test: add test deliveryDate with isRush is true and deliveryState is MA

---
+++ test/deliveryTest.js
@@ -0,0 +1,19 @@
+const deliveryTest = require('ava');
+const {deliveryDate} = require('../src/delivery');
+
+function plusDays (time) {
+    return time + 1;
+}
+
+deliveryTest('test deliveryDate with isRush is true and deliveryState is MA', t => {
+    const isRush = true;
+    const anOrder = {
+        deliveryState: 'MA',
+        placedOn: {
+            plusDays: plusDays
+        }
+    }
+
+    const result = deliveryDate(anOrder, isRush);
+    t.is(result, 3);
+})
