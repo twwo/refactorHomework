e55fadf835f3e9c018814028ca566b80b897f3f3
LISH5 <727904507@qq.com>
2020-08-28 08:33:11 +0800

    [homework][Shao] test: add test employee with name Shao and type unknown

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -6,3 +6,12 @@ employeeTest('test employee with name Shao and type engineer', t => {
     const result = employee.toString();
     t.is(result, 'Shao (engineer)');
 })
+
+employeeTest('test employee with name Shao and type unknown', t => {
+    try {
+        const employee = createEmployee('Shao', 'unknown');
+        t.fail();
+    } catch (e) {
+        t.is(e.message, 'Employee cannot be of type unknown');
+    }
+})
