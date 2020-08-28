563ebab501bb440c1d1ea34f62d3d2b6852b2a96
LISH5 <727904507@qq.com>
2020-08-28 17:24:18 +0800

    [homework][Shao] test: add test deliveryDate with isRush is true and deliveryState is NH

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -17,3 +17,16 @@ deliveryTest('test deliveryDate with isRush is true and deliveryState is MA', t 
     const result = deliveryDate(anOrder, isRush);
     t.is(result, 3);
 })
+
+deliveryTest('test deliveryDate with isRush is true and deliveryState is NH', t => {
+    const isRush = true;
+    const anOrder = {
+        deliveryState: 'NH',
+        placedOn: {
+            plusDays: plusDays
+        }
+    }
+
+    const result = deliveryDate(anOrder, isRush);
+    t.is(result, 4);
+})
